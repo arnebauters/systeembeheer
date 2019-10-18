b069c0b65218e5512c1c79246d0d42a8011bbf59
arnebauters <arne.bauters@student.ucll.be>
2019-10-18 17:09:35 +0200

    small mistake in 2 sql scripts

--- scripts/check_max_nr_rows.sql
+++ scripts/check_max_nr_rows.sql
@@ -7,7 +7,7 @@ ON log FOR EACH ROW
 
 	BEGIN
 		SELECT COUNT(*) INTO @nr_rows FROM log;
-		IF @nr_rows > 100 THEN
+		IF @nr_rows >= 100 THEN
 			SIGNAL SQLSTATE '45000'
 				SET MESSAGE_TEXT = 'Cannot add row, max number of rows reached.';
 		END IF;
