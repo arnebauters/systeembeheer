00511db9741fd93917595476d59e327911b6e945
arnebauters <arne.bauters@student.ucll.be>
2019-10-18 13:54:55 +0200

    scripts to create triggers for checking rows in db

---
+++ scripts/check_min_nr_rows.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+
+USE check
+CREATE TRIGGER trigger2
+BEFORE DELETE
+ON log FOR EACH ROW
+
+	BEGIN
+		SELECT COUNT(*) INTO @nr_rows FROM log;
+		IF @nr_rows <= 80 THEN
+			SIGNAL SQLSTATE '45000'
+				SET MESSAGE_TEXT = 'Cannot delete row, min number of rows.';
+		END IF;
+	END; //
+
+DELIMITER ;
