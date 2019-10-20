0d426ac62f13ba4a02ca938134117aacf79c48bc
arnebauters <arne.bauters@student.ucll.be>
2019-10-20 10:56:22 +0200

    added script to delete rows from table when to full

---
+++ scripts/delete_rows.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+USE check
+SET GLOBAL event_scheduler = ON;
+CREATE EVENT delete_rows_when_table_to_full
+	ON SCHEDULE EVERY 1 HOUR STARTS '2019-10-20 11:00:00'
+	DO
+	BEGIN IF (SELECT COUNT(*) FROM log) > 90 THEN
+		DELETE FROM log ORDER BY id DESC limit 10;
+	END IF;
+END //
