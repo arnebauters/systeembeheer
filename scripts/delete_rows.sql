7d8cf6af55d6e5a361b7a6c9540a63d7a402bbff
arnebauters <arne.bauters@student.ucll.be>
2019-10-22 14:15:21 +0200

    made script to delete if max_rows reached

--- scripts/delete_rows.sql
+++ scripts/delete_rows.sql
@@ -1,6 +1,5 @@
-DELIMITER //
-USE check
-SET GLOBAL event_scheduler = ON;
+Terminal close -- exit!
+ET GLOBAL event_scheduler = ON;
 CREATE EVENT delete_rows_when_table_to_full
 	ON SCHEDULE EVERY 1 HOUR STARTS '2019-10-20 11:00:00'
 	DO
